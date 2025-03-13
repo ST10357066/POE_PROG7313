a93e9a92ff3ecf7f00b286a06e6b777ac66d30db
ST10357066 <128793955+ST10357066@users.noreply.github.com>
2025-03-13 11:08:15 +0200

    Update User.kt

--- app/src/main/java/vcmsa/projects/poe_prog7313/dataClasses/User.kt
+++ app/src/main/java/vcmsa/projects/poe_prog7313/dataClasses/User.kt
@@ -1,3 +1,9 @@
 package vcmsa.projects.prog7313_poe.DataClass
 
-data class User()
+data class User(
+    val userId: Int,                  // Primary Key, auto-increment
+    val username: String,             // Unique username
+    val passwordHash: String,         // Hashed password
+    val email: String? = null,        // Optional email for notifications or recovery
+    val createdAt: LocalDateTime      // Timestamp when the account was created
+)
